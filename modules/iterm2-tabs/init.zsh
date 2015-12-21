7f3d37ba81415bdcf29efe947a40fc4474462238
Brett Taylor <brett.taylor@area360.com>
2015-12-21 14:50:28 +1300

    Pluralised the name of the functions.

--- modules/iterm2-tabs/init.zsh
+++ modules/iterm2-tabs/init.zsh
@@ -24,46 +24,47 @@ if [[ -n "$ITERM_SESSION_ID" ]]; then
   tab-other()   { tab-color 105 105 105 } # dimgrey
   tab-reset()   { echo -ne "\033]6;1;bg;*;default\a" }
 
-  function iterm2_tab_precmd() {
+  function iterm2_tabs_precmd() {
     tab-reset
   }
 
-  function iterm2_tab_preexec() {
-    if zstyle -t ':prezto:module:iterm2' dim-active-tabs; then
-    tab-other
-
-    zstyle -a ':prezto:module:iterm2' info 'info_regexps'
-    for info_regexp ("$info_regexps[@]"); do
-      iterm2_tab $1 $info_regexp info
-    done
-    unset info_regexps{s,}
-
-    zstyle -a ':prezto:module:iterm2' safe 'safe_regexps'
-    for safe_regexp ("$safe_regexps[@]"); do
-      iterm2_tab $1 $safe_regexp safe
-    done
-    unset safe_regexps{s,}
-
-    zstyle -a ':prezto:module:iterm2' warning 'warning_regexps'
-    for warning_regexp ("$warning_regexps[@]"); do
-      iterm2_tab $1 $warning_regexp warning
-    done
-    unset warning_regexps{s,}
-
-    zstyle -a ':prezto:module:iterm2' danger 'danger_regexps'
-    for danger_regexp ("$danger_regexps[@]"); do
-      iterm2_tab $1 $danger_regexp danger
-    done
-    unset danger_regexps{s,}
-  }
-
-  function iterm2_tab() {
+  function iterm2_tabs() {
     if [[ "$1" =~ "$2" ]]; then
       tab-$3
     fi
   }
 
+  function iterm2_tabs_preexec() {
+    if zstyle -t ':prezto:module:iterm2' dim-active-tabs; then
+      tab-other
+    fi
+
+    zstyle -a ':prezto:module:iterm2' info 'info_regexps'
+    for info_regexp ("$info_regexps[@]"); do
+      iterm2_tabs $1 $info_regexp info
+    done
+    unset info_regexps{s,}
+
+    zstyle -a ':prezto:module:iterm2' safe 'safe_regexps'
+    for safe_regexp ("$safe_regexps[@]"); do
+      iterm2_tabs $1 $safe_regexp safe
+    done
+    unset safe_regexps{s,}
+
+    zstyle -a ':prezto:module:iterm2' warning 'warning_regexps'
+    for warning_regexp ("$warning_regexps[@]"); do
+      iterm2_tabs $1 $warning_regexp warning
+    done
+    unset warning_regexps{s,}
+
+    zstyle -a ':prezto:module:iterm2' danger 'danger_regexps'
+    for danger_regexp ("$danger_regexps[@]"); do
+      iterm2_tabs $1 $danger_regexp danger
+    done
+    unset danger_regexps{s,}
+  }
+
   autoload -U add-zsh-hook
-  add-zsh-hook precmd  iterm2_tab_precmd
-  add-zsh-hook preexec iterm2_tab_preexec
+  add-zsh-hook precmd  iterm2_tabs_precmd
+  add-zsh-hook preexec iterm2_tabs_preexec
 fi
